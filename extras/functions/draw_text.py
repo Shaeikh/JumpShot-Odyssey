0fc58a47e118c737e6b3fa6f1e590b0ec5693acc
Shæikh <51645154+Shaeikh@users.noreply.github.com>
2023-08-26 20:19:49 +0500

    Delete extras directory

--- extras/functions/draw_text.py
+++
@@ -1,7 +0,0 @@
-def draw_text(screen, text, font, color, x, y, shadow=False, shadow_color=(0, 0, 0)):
-    if shadow:
-        
-        text_obj = font.render(text, True, shadow_color)
-        screen.blit(text_obj, (x + 2, y + 2))
-    text_obj = font.render(text, True, color)
-    screen.blit(text_obj, (x, y))
